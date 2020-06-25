65738e7975aee25a176fab6ba0892452271192cc
Daksh Khetan <2012daksh@gmail.com>
2020-06-25 19:21:40 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Slack (clone)
+
+> _This is a clone of Slack chat app made using React._
+
+### To run the project locally on your machine:
+
+- Install dependencies by running `yarn` command in the terminal.
+- Run the app in the development mode by running command `yarn start`.
+- Open `http://localhost:3000` to view it in the browser.
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import registerServiceWorker from "./registerServiceWorker";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
+registerServiceWorker();
