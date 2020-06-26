c4660badf3ca340c10a1486be3f295f9f8a18b6a
Daksh Khetan <2012daksh@gmail.com>
2020-06-26 17:50:29 +0530

    redirect to app component if signed-in

--- src/index.js
+++ src/index.js
@@ -4,20 +4,44 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from 'react-router-dom';
 
-const Root = () => (
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.history.push('/');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
   <Router>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} />
-    </Switch>
-  </Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById('root')
 );
 
-ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
