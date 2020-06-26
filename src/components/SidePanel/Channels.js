f9467874239831d939f3f4d2c26c75052b15399a
Daksh Khetan <2012daksh@gmail.com>
2020-06-27 01:36:56 +0530

    remove firebase listeners on unmount

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -20,12 +20,20 @@ class Channels extends React.Component {
     this.addListeners();
   }
 
+  componentWillUnmount() {
+    this.removeListeners();
+  }
+
   addListeners = () => {
     let loadedChannels = [];
     this.state.channelsRef.on('child_added', (snap) => {
       loadedChannels.push(snap.val());
       this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
     });
+  };
+
+  removeListeners = () => {
+    this.state.channelsRef.off();
   };
 
   setFirstChannel = () => {
