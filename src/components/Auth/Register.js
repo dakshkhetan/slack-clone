649d352288a2b985c2d2da25d3e2f3820637d1cb
Daksh Khetan <2012daksh@gmail.com>
2020-06-26 17:40:14 +0530

    create login form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -133,7 +133,7 @@ class Register extends React.Component {
     return (
       <Grid textAlign='center' verticalAlign='middle' className='app'>
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as='h2' icon color='orange' textAlign='center'>
+          <Header as='h1' icon color='orange' textAlign='center'>
             <Icon name='puzzle piece' color='orange' />
             Register for DevChat
           </Header>
