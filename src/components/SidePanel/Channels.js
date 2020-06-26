4cb7f7dd55c0168d1552a5fb82dcac537c794217
Daksh Khetan <2012daksh@gmail.com>
2020-06-27 01:05:08 +0530

    create & add new channels to database

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,17 +1,58 @@
 import React from 'react';
+import firebase from '../../firebase';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 class Channels extends React.Component {
   state = {
+    user: this.props.currentUser,
     channels: [],
     channelName: '',
     channelDetails: '',
+    channelsRef: firebase.database().ref('channels'),
     modal: false
+  };
+
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails, user } = this.state;
+
+    const key = channelsRef.push().key;
+
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL
+      }
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: '', channelDetails: '' });
+        this.closeModal();
+        console.log('channel added');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.addChannel();
+    }
   };
 
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
 
   openModal = () => this.setState({ modal: true });
 
@@ -36,7 +77,7 @@ class Channels extends React.Component {
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header>Add a Channel</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
                 <Input
                   fluid
@@ -58,7 +99,7 @@ class Channels extends React.Component {
           </Modal.Content>
 
           <Modal.Actions>
-            <Button color='green' inverted>
+            <Button color='green' inverted onClick={this.handleSubmit}>
               <Icon name='checkmark' /> Add
             </Button>
             <Button color='red' inverted onClick={this.closeModal}>
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -16,7 +16,7 @@ class SidePanel extends React.Component {
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
       >
         <UserPanel currentUser={currentUser} />
-        <Channels />
+        <Channels currentUser={currentUser} />
       </Menu>
     );
   }
