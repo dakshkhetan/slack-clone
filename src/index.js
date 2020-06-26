7df5691ad0ae3345050143da5023ce2d10f7dda4
Daksh Khetan <2012daksh@gmail.com>
2020-06-26 20:42:36 +0530

    setup redux & create setUser action

--- src/index.js
+++ src/index.js
@@ -15,10 +15,20 @@ import {
   withRouter
 } from 'react-router-dom';
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        // console.log(user);
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     });
@@ -35,13 +45,14 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
-
 registerServiceWorker();
