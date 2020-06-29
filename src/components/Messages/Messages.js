e1ff4831b870c37baf40b730bd935eba6728af10
Daksh Khetan <2012daksh@gmail.com>
2020-06-29 21:16:45 +0530

    implement search messages functionality

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -13,7 +13,10 @@ class Messages extends React.Component {
     messagesLoading: true,
     channel: this.props.currentChannel,
     user: this.props.currentUser,
-    numUniqueUsers: ''
+    numUniqueUsers: '',
+    searchTerm: '',
+    searchLoading: false,
+    searchResults: []
   };
 
   componentDidMount() {
@@ -38,6 +41,32 @@ class Messages extends React.Component {
       });
       this.countUniqueUsers(loadedMessages);
     });
+  };
+
+  handleSearchChange = (event) => {
+    this.setState(
+      {
+        searchTerm: event.target.value,
+        searchLoading: true
+      },
+      () => this.handleSearchMessages()
+    );
+  };
+
+  handleSearchMessages = () => {
+    const channelMessages = [...this.state.messages];
+    const regex = new RegExp(this.state.searchTerm, 'gi');
+    const searchResults = channelMessages.reduce((acc, message) => {
+      if (
+        (message.content && message.content.match(regex)) ||
+        message.user.name.match(regex)
+      ) {
+        acc.push(message);
+      }
+      return acc;
+    }, []);
+    this.setState({ searchResults });
+    setTimeout(() => this.setState({ searchLoading: false }), 1000);
   };
 
   countUniqueUsers = (messages) => {
@@ -65,18 +94,23 @@ class Messages extends React.Component {
   displayChannelName = (channel) => (channel ? `#${channel.name}` : '');
 
   render() {
-    const { messagesRef, messages, channel, user, numUniqueUsers } = this.state;
+    // prettier-ignore
+    const { messagesRef, messages, channel, user, numUniqueUsers, searchTerm, searchResults, searchLoading } = this.state;
 
     return (
       <React.Fragment>
         <MessagesHeader
           channelName={this.displayChannelName(channel)}
           numUniqueUsers={numUniqueUsers}
+          handleSearchChange={this.handleSearchChange}
+          searchLoading={searchLoading}
         />
 
         <Segment>
           <Comment.Group className='messages'>
-            {this.displayMessages(messages)}
+            {searchTerm
+              ? this.displayMessages(searchResults)
+              : this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
 
--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,7 +3,12 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 
 class MessagesHeader extends React.Component {
   render() {
-    const { channelName, numUniqueUsers } = this.props;
+    const {
+      channelName,
+      numUniqueUsers,
+      handleSearchChange,
+      searchLoading
+    } = this.props;
 
     return (
       <Segment clearing>
@@ -19,6 +24,8 @@ class MessagesHeader extends React.Component {
         {/* Channel Search Input */}
         <Header floated='right'>
           <Input
+            loading={searchLoading}
+            onChange={handleSearchChange}
             size='mini'
             icon='search'
             name='searchTerm'
