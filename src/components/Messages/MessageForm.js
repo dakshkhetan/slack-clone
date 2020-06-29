0d7e883f7d3aebde6d6d4f562402a0afbaf47179
Daksh Khetan <2012daksh@gmail.com>
2020-06-29 17:36:30 +0530

    show file upload progress bar

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -4,6 +4,7 @@ import firebase from '../../firebase';
 import { Segment, Button, Input } from 'semantic-ui-react';
 
 import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
   state = {
@@ -135,7 +136,8 @@ class MessageForm extends React.Component {
   };
 
   render() {
-    const { errors, message, loading, modal } = this.state;
+    // prettier-ignore
+    const { errors, message, loading, modal, uploadState, percentUploaded } = this.state;
 
     return (
       <Segment className='message__form'>
@@ -170,12 +172,16 @@ class MessageForm extends React.Component {
             labelPosition='right'
             icon='cloud upload'
           />
-          <FileModal
-            modal={modal}
-            closeModal={this.closeModal}
-            uploadFile={this.uploadFile}
-          />
         </Button.Group>
+        <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
+          uploadFile={this.uploadFile}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     );
   }
---
+++ src/components/Messages/ProgressBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Progress } from 'semantic-ui-react';
+
+const ProgressBar = ({ uploadState, percentUploaded }) =>
+  uploadState && (
+    <Progress
+      className='progress__bar'
+      percent={percentUploaded}
+      progress
+      indicating
+      size='medium'
+      inverted
+    />
+  );
+
+export default ProgressBar;
