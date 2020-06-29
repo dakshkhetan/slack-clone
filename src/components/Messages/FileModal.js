3b46fa459faa0f81174fbc4fec12465d2aff54a1
Daksh Khetan <2012daksh@gmail.com>
2020-06-29 14:14:48 +0530

    validate uploaded file mime-type

--- src/components/Messages/FileModal.js
+++ src/components/Messages/FileModal.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import mime from 'mime-types';
 import { Modal, Input, Button, Icon } from 'semantic-ui-react';
 
 class FileModal extends React.Component {
+  state = {
+    file: null,
+    authorized: ['image/jpeg', 'image/png']
+  };
+
+  addFile = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      this.setState({ file });
+    }
+  };
+
+  sendFile = () => {
+    const { file } = this.state;
+    const { uploadFile, closeModal } = this.props;
+
+    if (file !== null) {
+      if (this.isAuthorized(file.name)) {
+        const metadata = { contentType: mime.lookup(file.name) };
+        uploadFile(file, metadata);
+        closeModal();
+        this.clearFile();
+      }
+    }
+  };
+
+  isAuthorized = (filename) =>
+    this.state.authorized.includes(mime.lookup(filename));
+
+  clearFile = () => this.setState({ file: null });
+
   render() {
     const { modal, closeModal } = this.props;
 
@@ -9,10 +41,16 @@ class FileModal extends React.Component {
       <Modal basic open={modal} onClose={closeModal}>
         <Modal.Header>Select an Image File</Modal.Header>
         <Modal.Content>
-          <Input fluid label='File types: jpg, png' name='file' type='file' />
+          <Input
+            onChange={this.addFile}
+            fluid
+            label='File types: jpg, png'
+            name='file'
+            type='file'
+          />
         </Modal.Content>
         <Modal.Actions>
-          <Button color='green' inverted>
+          <Button onClick={this.sendFile} color='green' inverted>
             <Icon name='checkmark' /> Send
           </Button>
           <Button color='red' inverted onClick={closeModal}>
--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -62,6 +62,10 @@ class MessageForm extends React.Component {
     }
   };
 
+  uploadFile = (file, metadata) => {
+    console.log(file, metadata);
+  };
+
   render() {
     const { errors, message, loading, modal } = this.state;
 
@@ -98,7 +102,11 @@ class MessageForm extends React.Component {
             labelPosition='right'
             icon='cloud upload'
           />
-          <FileModal modal={modal} closeModal={this.closeModal} />
+          <FileModal
+            modal={modal}
+            closeModal={this.closeModal}
+            uploadFile={this.uploadFile}
+          />
         </Button.Group>
       </Segment>
     );
