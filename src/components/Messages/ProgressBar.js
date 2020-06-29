bb1e511cf2f02c0e54f974d9ea772523035c58c6
Daksh Khetan <2012daksh@gmail.com>
2020-06-29 17:42:47 +0530

    hide progress bar when finished uploading

--- src/components/Messages/ProgressBar.js
+++ src/components/Messages/ProgressBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Progress } from 'semantic-ui-react';
 
 const ProgressBar = ({ uploadState, percentUploaded }) =>
-  uploadState && (
+  uploadState === 'uploading' && (
     <Progress
       className='progress__bar'
       percent={percentUploaded}
