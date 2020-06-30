ba08be43416c5d42e047e7171912b2c0cc3776f5
Daksh Khetan <2012daksh@gmail.com>
2020-06-30 20:29:40 +0530

    show users presence state in direct-messages component

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import firebase from '../../firebase';
 import { Menu, Icon } from 'semantic-ui-react';
 
 class DirectMessages extends React.Component {
   state = {
-    users: []
+    user: this.props.currentUser,
+    users: [],
+    usersRef: firebase.database().ref('users'),
+    connectedRef: firebase.database().ref('.info/connected'),
+    presenceRef: firebase.database().ref('presence')
   };
+
+  componentDidMount() {
+    if (this.state.user) {
+      this.addListeners(this.state.user.uid);
+    }
+  }
+
+  addListeners = (currentUserUid) => {
+    let loadedUsers = [];
+    this.state.usersRef.on('child_added', (snap) => {
+      if (currentUserUid !== snap.key) {
+        let user = snap.val();
+        user['uid'] = snap.key;
+        user['status'] = 'offline';
+        loadedUsers.push(user);
+        this.setState({ users: loadedUsers });
+      }
+    });
+
+    this.state.connectedRef.on('value', (snap) => {
+      if (snap.val() === true) {
+        const ref = this.state.presenceRef.child(currentUserUid);
+        ref.set(true);
+        ref.onDisconnect().remove((err) => {
+          if (err !== null) {
+            console.error(err);
+          }
+        });
+      }
+    });
+
+    this.state.presenceRef.on('child_added', (snap) => {
+      if (currentUserUid !== snap.key) {
+        this.addStatusToUser(snap.key);
+      }
+    });
+
+    this.state.presenceRef.on('child_removed', (snap) => {
+      if (currentUserUid !== snap.key) {
+        this.addStatusToUser(snap.key, false);
+      }
+    });
+  };
+
+  addStatusToUser = (userId, connected = true) => {
+    const updatedUsers = this.state.users.reduce((acc, user) => {
+      if (user.uid === userId) {
+        user['status'] = `${connected ? 'online' : 'offline'}`;
+      }
+      return acc.concat(user);
+    }, []);
+    this.setState({ users: updatedUsers });
+  };
+
+  isUserOnline = (user) => user.status === 'online';
 
   render() {
     const { users } = this.state;
@@ -17,7 +77,19 @@ class DirectMessages extends React.Component {
           </span>{' '}
           ({users.length})
         </Menu.Item>
-        {/* Users to Send Direct Messages */}
+        {users.map((user) => (
+          <Menu.Item
+            key={user.uid}
+            onClick={() => console.log(user)}
+            style={{ opacity: 0.7, fontStyle: 'italic' }}
+          >
+            <Icon
+              name='circle'
+              color={this.isUserOnline(user) ? 'green' : 'red'}
+            />
+            @ {user.name}
+          </Menu.Item>
+        ))}
       </Menu.Menu>
     );
   }
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -19,7 +19,7 @@ class SidePanel extends React.Component {
       >
         <UserPanel currentUser={currentUser} />
         <Channels currentUser={currentUser} />
-        <DirectMessages />
+        <DirectMessages currentUser={currentUser} />
       </Menu>
     );
   }
