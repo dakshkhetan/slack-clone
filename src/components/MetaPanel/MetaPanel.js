22cbab317aa79510927a9aea4a9b5d4a480ec3c4
Daksh Khetan <2012daksh@gmail.com>
2020-07-03 13:01:52 +0530

    create scaffold meta-panel component

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,68 @@
 import React from 'react';
+import { Segment, Accordion, Header, Icon } from 'semantic-ui-react';
 
 class MetaPanel extends React.Component {
+  state = {
+    activeIndex: 0
+  };
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <div>MetaPanel</div>;
+    const { activeIndex } = this.state;
+
+    return (
+      <Segment>
+        <Header as='h3' attached='top'>
+          About # Channel
+        </Header>
+        <Accordion styled attached='true'>
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='info' />
+            Channel Details
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            details
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='user circle' />
+            Top Posters
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            posters
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='pencil alternate' />
+            Created By
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            creator
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
