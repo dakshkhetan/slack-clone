dedd37574b581ebebafe2c929565786eca510523
Daksh Khetan <2012daksh@gmail.com>
2020-07-04 17:40:19 +0530

    display saved colors in color-panel

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -10,7 +10,22 @@ class ColorPanel extends React.Component {
     primary: '',
     secondary: '',
     user: this.props.currentUser,
-    usersRef: firebase.database().ref('users')
+    usersRef: firebase.database().ref('users'),
+    userColors: []
+  };
+
+  componentDidMount() {
+    if (this.state.user) {
+      this.addListener(this.state.user.uid);
+    }
+  }
+
+  addListener = (userId) => {
+    let userColors = [];
+    this.state.usersRef.child(`${userId}/colors`).on('child_added', (snap) => {
+      userColors.unshift(snap.val());
+      this.setState({ userColors });
+    });
   };
 
   handleChangePrimary = (color) => this.setState({ primary: color.hex });
@@ -38,12 +53,28 @@ class ColorPanel extends React.Component {
       .catch((err) => console.error(err));
   };
 
+  displayUserColors = (colors) =>
+    colors.length > 0 &&
+    colors.map((color, i) => (
+      <React.Fragment key={i}>
+        <Divider />
+        <div className='color__container'>
+          <div className='color__square' style={{ background: color.primary }}>
+            <div
+              className='color__overlay'
+              style={{ background: color.secondary }}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    ));
+
   openModal = () => this.setState({ modal: true });
 
   closeModal = () => this.setState({ modal: false });
 
   render() {
-    const { modal, primary, secondary } = this.state;
+    const { modal, primary, secondary, userColors } = this.state;
 
     return (
       <Sidebar
@@ -56,6 +87,7 @@ class ColorPanel extends React.Component {
       >
         <Divider />
         <Button icon='add' size='small' color='blue' onClick={this.openModal} />
+        {this.displayUserColors(userColors)}
 
         {/* Color Picker Modal */}
         <Modal basic open={modal} onClose={this.closeModal}>
