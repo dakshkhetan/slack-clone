fa2239ebc1584bf36fa33c975d4fab048356508d
Daksh Khetan <2012daksh@gmail.com>
2020-07-12 23:23:31 +0530

    add option to preview cropped avatar in modal

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -8,7 +8,9 @@ class UserPanel extends React.Component {
   state = {
     user: this.props.currentUser,
     modal: false,
-    previewImage: ''
+    previewImage: '',
+    croppedImage: '',
+    blob: ''
   };
 
   openModal = () => this.setState({ modal: true });
@@ -47,6 +49,18 @@ class UserPanel extends React.Component {
     }
   };
 
+  handleCropImage = () => {
+    if (this.avatarEditor) {
+      this.avatarEditor.getImageScaledToCanvas().toBlob((blob) => {
+        let imageUrl = URL.createObjectURL(blob);
+        this.setState({
+          croppedImage: imageUrl,
+          blob
+        });
+      });
+    }
+  };
+
   handleSignout = () => {
     firebase
       .auth()
@@ -55,7 +69,7 @@ class UserPanel extends React.Component {
   };
 
   render() {
-    const { user, modal, previewImage } = this.state;
+    const { user, modal, previewImage, croppedImage } = this.state;
     const { primaryColor } = this.props;
 
     return (
@@ -98,6 +112,7 @@ class UserPanel extends React.Component {
                   <Grid.Column className='ui center aligned grid'>
                     {previewImage && (
                       <AvatarEditor
+                        ref={(node) => (this.avatarEditor = node)}
                         image={previewImage}
                         width={120}
                         height={120}
@@ -106,15 +121,26 @@ class UserPanel extends React.Component {
                       />
                     )}
                   </Grid.Column>
-                  <Grid.Column>{/* Cropped Image Preview */}</Grid.Column>
+                  <Grid.Column>
+                    {croppedImage && (
+                      <Image
+                        style={{ margin: '3.5em auto' }}
+                        width={100}
+                        height={100}
+                        src={croppedImage}
+                      />
+                    )}
+                  </Grid.Column>
                 </Grid.Row>
               </Grid>
             </Modal.Content>
             <Modal.Actions>
-              <Button color='green' inverted>
-                <Icon name='save' /> Change Avatar
-              </Button>
-              <Button color='green' inverted>
+              {croppedImage && (
+                <Button color='green' inverted>
+                  <Icon name='save' /> Change Avatar
+                </Button>
+              )}
+              <Button color='green' inverted onClick={this.handleCropImage}>
                 <Icon name='image' /> Preview
               </Button>
               <Button color='red' inverted onClick={this.closeModal}>
