af77ef0eeabf162e7d775654be92e952b44c5313
Daksh Khetan <2012daksh@gmail.com>
2020-07-14 16:27:28 +0530

    cancel any upload tasks (in firebase) on unmount

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -24,6 +24,13 @@ class MessageForm extends React.Component {
     emojiPicker: false
   };
 
+  componentWillUnmount() {
+    if (this.state.uploadTask !== null) {
+      this.state.uploadTask.cancel();
+      this.setState({ uploadTask: null });
+    }
+  }
+
   openModal = () => this.setState({ modal: true });
 
   closeModal = () => this.setState({ modal: false });
